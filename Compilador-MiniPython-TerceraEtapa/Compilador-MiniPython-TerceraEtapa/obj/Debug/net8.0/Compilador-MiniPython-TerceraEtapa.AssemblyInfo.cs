0c1072a8d0009bfbd0910fbf5a4db1ea7822fb9b
Brayan Gutierrez Dinarte <38222878+BrayanD17@users.noreply.github.com>
2024-11-02 21:20:21 -0600

    Update

--- Compilador-MiniPython-TerceraEtapa/Compilador-MiniPython-TerceraEtapa/obj/Debug/net8.0/Compilador-MiniPython-TerceraEtapa.AssemblyInfo.cs
+++ Compilador-MiniPython-TerceraEtapa/Compilador-MiniPython-TerceraEtapa/obj/Debug/net8.0/Compilador-MiniPython-TerceraEtapa.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Compilador-MiniPython-TerceraEtapa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2f2f4a7c7c6b49fc910231a6daa7175ba2b0032")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6ae6cd8cbcd26f7300b3e6f63fd95e2e8931a39")]
 [assembly: System.Reflection.AssemblyProductAttribute("Compilador-MiniPython-TerceraEtapa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Compilador-MiniPython-TerceraEtapa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
